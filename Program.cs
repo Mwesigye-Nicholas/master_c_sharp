f444297a5705dff39e97710ac6b9bada350e1c48
Mwesigye_Nicholas <nicholastoshack12345@gmail.com>
2025-09-10 13:05:57 -0600

    decision logic

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Random coin = new  Random();
+int choices = coin.Next(0, 2);
+string result = choices > 1 ? "heads" : "tails";
+Console.WriteLine(result);
+
+
+//* Another project to reinforce conditional branching concept.
+string permission = "Admin | Manager";
+
+int level = 55;
+
+if (permission.Contains("Admin") && level > 55)
+    Console.WriteLine("Welcome, Super Admin user.");
+else if (permission.Contains("Admin") && level <= 55)
+    Console.WriteLine("Welcome, Admin user.");
+else if (permission.Contains("Manager") && (level == 20 || level > 20))
+    Console.WriteLine("Contact an Admin for access");
+else if (permission.Contains("Manager") && level < 20)
+    Console.WriteLine("You don't have sufficient privileges");
+else
+    Console.WriteLine("You don't have sufficient privileges");
